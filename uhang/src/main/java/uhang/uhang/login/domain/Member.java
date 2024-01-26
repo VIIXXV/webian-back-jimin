4fcbf816541df907809b759d28897cd9d9fb4812
VIIXXV <jiminiyooni@sookmyung.ac.kr>
2024-01-27 05:31:26 +0900

    posting 기능 위한 post(member와 연결되어있으므로 member도 함께) 엔티티 매핑

--- uhang/src/main/java/uhang/uhang/login/domain/Member.java
+++ uhang/src/main/java/uhang/uhang/login/domain/Member.java
@@ -1,6 +1,24 @@
 package uhang.uhang.login.domain;
 
-public class Member {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import uhang.uhang.posting.domain.Post;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "members")
+@Getter @Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "memberId")
+    private String memberEmail;
+    private String memberPw;
+    private int writeAuth;
+    private Long studNum;
+    @OneToMany(mappedBy = "memberId")
+    private List<Post> posts = new ArrayList<>();
 
 }
